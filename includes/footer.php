25202c1c8afdddc424dc496f3b282f710d923512
Shokunbi Johnson <maximusjshokes@gmail.com>
2020-09-10 22:40:31 +0100

    Updated includes

---
+++ includes/footer.php
@@ -0,0 +1,53 @@
+<footer>
+    <div class="copyright_wrap copyright_style_text scheme_original">
+        <div class="copyright_wrap_inner">
+            <div class="content_wrap">
+                <div class="copyright_text">GSF FUNAAB RESOURCE CENTER © 2020 All Rights Reserved. Developed by <a href="./resourcecenter.php">Academic Unit 19/20</a>
+                </div>
+                <div class="copyright_text"><i>Disclaimer: Files uploaded on this website is not a property of GSF FUNAAB Resource Center. Should any file infringe on copywrite licence agreement, <a href="./reportfile.php">Report to the admin</a></i>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- <script type="text/javascript" src="./GSF FUNAAB_files/_packed.js"></script>-->
+
+
+<script type="text/javascript" src="./GSF FUNAAB_files/jquery.js"></script>
+<script type="text/javascript" src="./GSF FUNAAB_files/jquery-migrate.min.js"></script>
+
+<script type="text/javascript" src="./GSF FUNAAB_files/modernizr.min.js"></script>
+<script type="text/javascript" src="./GSF FUNAAB_files/_main.min.js"></script>
+<script type="text/javascript" src="./GSF FUNAAB_files/_packed.js"></script>
+
+<!-- <script type="text/javascript" src="./GSF FUNAAB_files/lightbox.js"></script>
+<script type="text/javascript" src="./GSF FUNAAB_files/jquery.themepunch.tools.min.js"></script>
+<script type="text/javascript" src="./GSF FUNAAB_files/jquery.themepunch.essential.min.js"></script>
+<script type="text/javascript" src="./GSF FUNAAB_files/essential-grid-homepage-1.min.js"></script> -->
+
+<!-- <script type="text/javascript" src="./GSF FUNAAB_files/jquery.themepunch.revolution.min.js"></script> -->
+
+<!-- <script type="text/javascript" src="./GSF FUNAAB_files/revolution.extension.slideanims.min.js"></script>
+<script type="text/javascript" src="./GSF FUNAAB_files/revolution.extension.layeranimation.min.js"></script>
+<script type="text/javascript" src="./GSF FUNAAB_files/revolution.extension.navigation.min.js"></script>
+<script type="text/javascript" src="./GSF FUNAAB_files/revslider-homepage-1.min.js"></script> -->
+
+<!-- <script type="text/javascript" src="./GSF FUNAAB_files/jquery.jplayer.js"></script>
+<script type="text/javascript" src="./GSF FUNAAB_files/ttw-musicplayer-homepage-1.min.js"></script> -->
+
+<script type="text/javascript" src="./GSF FUNAAB_files/superfish.min.js"></script>
+<script type="text/javascript" src="./GSF FUNAAB_files/core.utils.min.js"></script>
+<script type="text/javascript" src="./GSF FUNAAB_files/core.init.min.js"></script>
+<script type="text/javascript" src="./GSF FUNAAB_files/template.init.min.js"></script>
+
+<!-- <script type="text/javascript" src="./GSF FUNAAB_files/shortcodes.min.js"></script>
+<script type="text/javascript" src="./GSF FUNAAB_files/core.messages.min.js"></script>
+<script type="text/javascript" src="./GSF FUNAAB_files/jquery.magnific-popup.min.js"></script> -->
+
+<!-- <script type="text/javascript" src="./GSF FUNAAB_files/jquery.plugin.min.js"></script>
+<script type="text/javascript" src="./GSF FUNAAB_files/jquery.countdown.min.js"></script>
+<script type="text/javascript" src="./GSF FUNAAB_files/swiper.min.js"></script>
+<script type="text/javascript" src="./GSF FUNAAB_files/isotope.pkgd.min.js"></script> -->
+</body>
+
+</html>
