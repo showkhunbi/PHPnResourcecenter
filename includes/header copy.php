7fcb131c27f1c798a7c9414afb31fc6f1a12e42a
Shokunbi Johnson <maximusjshokes@gmail.com>
2020-09-11 22:40:42 +0100

    Updated includes

---
+++ includes/header copy.php
@@ -0,0 +1,227 @@
+<!DOCTYPE html>
+<html lang="en-US" class="scheme_original">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="format-detection" content="telephone=no">
+
+    <title>GSF FUNAAB</title>
+
+    <link rel="stylesheet" href="./GSF FUNAAB_files/css">
+    <link rel="stylesheet" href="./GSF FUNAAB_files/fontello.css" type="text/css" media="all">
+
+    <link rel="stylesheet" href="./GSF FUNAAB_files/settings.css" type="text/css" media="all">
+    <link rel="stylesheet" href="./GSF FUNAAB_files/lightbox.css" type="text/css" media="all">
+    <link rel="stylesheet" href="./GSF FUNAAB_files/tpl-essential-grids.min.css" type="text/css" media="all">
+
+    <link rel="stylesheet" href="./GSF FUNAAB_files/settings(1).css" type="text/css" media="all">
+    <link rel="stylesheet" href="./GSF FUNAAB_files/plugin.revslider.min.css" type="text/css" media="all">
+
+    <link rel="stylesheet" href="./GSF FUNAAB_files/style.min.css" type="text/css" media="all">
+    <link rel="stylesheet" href="./GSF FUNAAB_files/core.animation.min.css" type="text/css" media="all">
+    <link rel="stylesheet" href="./GSF FUNAAB_files/shortcodes.min.css" type="text/css" media="all">
+
+    <link rel="stylesheet" href="./GSF FUNAAB_files/plugin.instagram-widget.min.css" type="text/css" media="all">
+    <link rel="stylesheet" href="./GSF FUNAAB_files/style.css" type="text/css" media="all">
+    <link rel="stylesheet" href="./GSF FUNAAB_files/plugin.html5-jquery-audio-player.min.css" type="text/css" media="all">
+
+    <link rel="stylesheet" href="./GSF FUNAAB_files/swiper.min.css" type="text/css" media="all">
+
+    <link rel="stylesheet" href="./GSF FUNAAB_files/skin.min.css" type="text/css" media="all">
+    <link rel="stylesheet" href="./GSF FUNAAB_files/responsive.min.css" type="text/css" media="all">
+
+    <link rel="stylesheet" href="./GSF FUNAAB_files/core.messages.min.css" type="text/css" media="all">
+    <link rel="stylesheet" href="./GSF FUNAAB_files/magnific-popup.min.css" type="text/css" media="all">
+    <link rel="stylesheet" href="./includes/resource_center_styles.css">
+
+    <link rel="icon" href="http://gsffunaab.com/images/index.jpg">
+    <link rel="icon" href="http://gsffunaab.com/images/index.jpg">
+    <link rel="apple-touch-icon-precomposed" href="http://gsffunaab.com/images/index.jpg">
+</head>
+
+<body class="home page body_style_wide body_filled article_style_stretch scheme_original top_panel_show top_panel_above sidebar_hide">
+    <a id="toc_home" class="sc_anchor" title="Home" data-description="&lt;i&gt;Return to Home&lt;/i&gt; - &lt;br&gt;navigate to home page of the site" data-icon="icon-home" data-url="index.html" data-separator="yes"></a>
+    <a id="toc_top" class="sc_anchor" title="To Top" data-description="&lt;i&gt;Back to top&lt;/i&gt; - &lt;br&gt;scroll to top of the page" data-icon="icon-double-up" data-url="" data-separator="yes"></a>
+    <!-- Body wrap -->
+    <div class="body_wrap">
+        <!-- Page wrap -->
+        <div class="page_wrap">
+            <div class="top_panel_fixed_wrap"></div>
+            <!-- Header -->
+            <header class="top_panel_wrap top_panel_style_3 scheme_original">
+                <div class="top_panel_wrap_inner top_panel_inner_style_3 top_panel_position_above">
+                    <!-- Menu area -->
+                    <div class="top_panel_middle">
+                        <div class="content_wrap">
+                            <!-- Contact logo -->
+                            <div class="contact_logo">
+                                <div class="logo">
+                                    <a href="http://gsffunaab.com/index.html">
+                                        <img src="./GSF FUNAAB_files/logo.png" class="logo_main" alt="">
+                                    </a>
+                                </div>
+                            </div>
+                            <!-- /Contact logo -->
+                            <!-- Main Menu -->
+                            <div class="menu_main_wrap">
+                                <nav class="menu_main_nav_area">
+                                    <ul id="menu_main" class="menu_main_nav">
+                                        <!-- Home -->
+                                        <li class="menu-item ">
+                                            <a href="http://gsffunaab.com/index.html">Home</a>
+
+                                            <!-- /Home -->
+                                            <!-- Pages -->
+                                        </li>
+                                        <li class="menu-item">
+                                            <a href="http://gsffunaab.com/about-us.html">About Us</a>
+
+                                            <!-- /Pages -->
+                                            <!-- Events -->
+                                        </li>
+                                        <li class="menu-item">
+                                            <a href="http://gsffunaab.com/page-404.html">Events</a>
+                                        </li>
+                                        <!-- /Events -->
+                                        <!-- Sermons -->
+                                        <li class="menu-item">
+                                            <a href="http://gsffunaab.com/sermons.html">Sermons</a>
+                                            <!-- /Sermons -->
+                                            <!-- Gallery -->
+                                        </li>
+                                        <li class="menu-item ">
+                                            <a href="http://gsffunaab.com/gallery.html">Gallery</a>
+
+                                            <!-- /Gallery -->
+                                            <!-- Blog -->
+                                        </li>
+                                        <li class="menu-item">
+                                            <a href="http://gsffunaab.com/gsf%20aspire.html">GSF Aspire</a>
+                                        </li>
+                                        <li class="menu-item">
+                                            <a href="http://gsffunaab.com/contacts.html">Contact Us</a>
+                                        </li>
+                                        <!-- /Blog -->
+                                        <!-- Donate -->
+                                        <li class="donate-btn menu-item">
+                                            <a href="http://gsffunaab.com/donation.html">Give</a>
+                                        </li>
+                                        <!-- /Donate -->
+                                    </ul>
+                                </nav>
+                            </div>
+                            <!-- /Main Menu -->
+                        </div>
+                    </div>
+                    <!-- /Menu area -->
+                </div>
+            </header>
+            <!-- /Header -->
+            <!-- Header Mobile -->
+            <div class="header_mobile">
+                <div class="content_wrap">
+                    <div class="menu_button icon-menu"></div>
+                    <div class="logo">
+                        <a href="http://gsffunaab.com/index.html">
+                            <img src="./GSF FUNAAB_files/logo.png" class="logo_main" alt=""> <br>
+                            <div class="gsf">GSF FUNAAB</div>
+                        </a>
+                    </div>
+                </div>
+                <div class="side_wrap">
+                    <div class="close">Close</div>
+                    <div class="panel_top">
+                        <nav class="menu_main_nav_area">
+                            <ul id="menu_main_mobile" class="menu_main_nav">
+                                <!-- Home -->
+                                <li class="menu-item ">
+                                    <a href="http://gsffunaab.com/index.html">Home</a>
+
+                                    <!-- /Home -->
+                                    <!-- Pages -->
+                                </li>
+                                <li class="menu-item ">
+                                    <a href="http://gsffunaab.com/about-us.html">About Us</a>
+                                    <!-- /Pages -->
+                                    <!-- Events -->
+                                </li>
+                                <li class="menu-item">
+                                    <a href="http://gsffunaab.com/page-404.html">Events</a>
+                                </li>
+                                <!-- /Events -->
+                                <!-- Sermons -->
+                                <li class="menu-item ">
+                                    <a href="http://gsffunaab.com/sermons.html">Sermons</a>
+
+                                    <!-- /Sermons -->
+                                    <!-- Gallery -->
+                                </li>
+                                <li class="menu-item ">
+                                    <a href="http://gsffunaab.com/gallery.html">Gallery</a>
+
+                                    <!-- /Gallery -->
+                                    <!-- Blog -->
+                                </li>
+                                <li class="menu-item">
+                                    <a href="http://gsffunaab.com/gsf%20aspire.html">GSF Aspire</a>
+                                </li>
+                                <li class="menu-item">
+                                    <a href="http://gsffunaab.com/contacts.html">Contact Us</a>
+                                </li>
+                                <!-- /Blog -->
+                                <!-- Donate -->
+                                <li class="donate-btn menu-item">
+                                    <a href="http://gsffunaab.com/donation.html">Give</a>
+                                </li>
+                                <!-- /Donate -->
+                            </ul>
+                        </nav>
+                        <!-- Search -->
+                        <div class="search_wrap search_style_regular search_state_fixed">
+                            <div class="search_form_wrap">
+                                <form role="search" method="get" class="search_form" action="http://gsffunaab.com/gsf%20aspire.html#">
+                                    <button type="submit" class="search_submit icon-search" title="Start search"></button>
+                                    <input type="text" class="search_field" placeholder="Search" value="" name="s">
+                                </form>
+                            </div>
+                            <div class="search_results widget_area scheme_original">
+                                <a class="search_results_close icon-cancel"></a>
+                                <div class="search_results_content"></div>
+                            </div>
+                        </div>
+                        <!-- /Search -->
+                        <!-- Login -->
+                        <div class="Login">
+                            <a href="http://gsffunaab.com/gsf%20aspire.html#popup_login" class="popup_link popup_login_link icon-user">Login</a>
+                        </div>
+                        <!-- /Login -->
+                    </div>
+                    <!-- Socials panel -->
+                    <div class="panel_bottom">
+                        <div class="contact_socials">
+                            <div class="sc_socials sc_socials_type_icons sc_socials_shape_square sc_socials_size_small">
+                                <div class="sc_socials_item">
+                                    <a href="https://www.facebook.com/gsffunaab" target="_blank" class="social_icons social_facebook">
+                                        <span class="icon-facebook"></span>
+                                    </a>
+                                </div>
+                                <div class="sc_socials_item">
+                                    <a href="https://www.twitter.com/gsffunaab" target="_blank" class="social_icons social_twitter">
+                                        <span class="icon-twitter"></span>
+                                    </a>
+                                </div>
+                                <div class="sc_socials_item">
+                                    <a href="https://www.facebook.com/gsffunaab" target="_blank" class="social_icons social_instagramm">
+                                        <span class="icon-instagramm"></span>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /Socials panel -->
+                </div>
+                <div class="mask"></div>
+            </div>
+            <div class="center-img.png"><a href="./resource center.php"><img src="./includes/resource-center-img.png" class="res-center-img" alt="Resource Center logo"></a></div>
