1686c5f481e3355299ab4f5f575d63c61cc8b0b6
Shokunbi Johnson <maximusjshokes@gmail.com>
2020-10-01 22:46:18 +0100

    Updated includes

---
+++ downloads.php
@@ -0,0 +1,62 @@
+<?php
+require_once "./includes/header.php";
+?>
+<div class="container2">
+    <p class="rec-p">Contribute to the Shared Resources</p>
+    <form action="./upload.php"><button class="rec-upload_botton">Upload file</button></form>
+</div>
+<div id="page-nav">
+    <ul>
+        <li id="li">
+            <a href="./resourcecenter.php">Home </a>
+        </li>
+    </ul>
+</div>
+<div class="container">
+    <div class=" downloads_page_container" id="container">
+        <!-- <div id="downloads_container">
+        <div id="files">
+            <div id="fileimg">
+                <img src="./gsf funaab_files/logo.png" alt="img">
+            </div>
+            <div id="filedad">
+                <div id="fileDetails">
+                    <div id="filename">
+                        <span id="spanimg"> <img src="./gsf funaab_files/logo.png" alt="img"></span>
+                        <h5>Uploaded File name.pdf</h5>
+                    </div>
+                    <div id="file_clickables">
+                        <p>Size: 4mb</p>
+                        <p>PRM</p>
+                        <p>100</p>
+                        <p>Note</p>
+                    </div>
+                    <div id="file_unclickables">
+                        <p>Uploaded by: Anonymous</p>
+                        <p>Date: 24/04/1998</p>
+                    </div>
+                </div>
+                <div id="ddiv"><a id="download-btn" href="#"><img src="./includes/images/download-img.png" alt="">Download</a></div>
+            </div>
+        </div>
+    </div> -->
+
+    </div>
+    <div id="pagination_nav_links">
+        <!-- <div id="pagination" class="pagination">
+                        <span>Page 1 of 385</span>
+                        <span class="current">1</span>
+                        <a href="https://delaniblog.com.ng/page/2/" class="inactive">2</a>
+                        <a class="btn btn-primary" href="https://delaniblog.com.ng/page/3/" class="inactive">3</a>
+                        <a href="https://delaniblog.com.ng/page/4/" class="inactive">4</a>
+                        <a href="https://delaniblog.com.ng/page/5/" class="inactive">5</a>
+                        <a href="https://delaniblog.com.ng/page/2/">Next ›</a>
+                        <a href="https://delaniblog.com.ng/page/385/">Last »</a>
+                    </div> -->
+    </div>
+</div>
+
+<script src="./includes/downloads_script.js"></script>
+<?php
+require_once "./includes/footer.php"
+?>
